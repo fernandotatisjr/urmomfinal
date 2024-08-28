0b09039f9da6120a20c84f43b845dd93a7ad542c
fernandotatisjr <alex.omalley44@gmail.com>
2024-08-27 19:05:05 -0500

    adding meta

--- includes/metaElements.js
+++ includes/metaElements.js
@@ -1,12 +1,17 @@
 class metaElements extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-21BM2L53NQ"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+                
+              gtag('config', 'G-21BM2L53NQ');
+            </script>
             <meta content="IE=edge" http-equiv="X-UA-Compatible">
             <meta content="width=device-width,initial-scale=1.0,maximum-scale=1" name="viewport">
             <meta content="telephone=no" name="format-detection">
-            <link rel="icon" href="" type="image/png">
-              <meta name="generator" content="">
-            <base href="">
             <meta http-equiv="content-type" content="text/html; charset=utf-8"></meta>`
     }
 }
