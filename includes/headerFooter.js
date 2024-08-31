f49fcfcbd9930c4319db7d8b73a817320804a899
fernandotatisjr <alex.omalley44@gmail.com>
2024-08-31 13:52:29 -0500

    adding new contact form and thank you page

--- includes/headerFooter.js
+++ includes/headerFooter.js
@@ -52,36 +52,17 @@ class specialFooter extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `
             <div id="footerWrapper">
-            <div id="mc_embed_shell">
-      <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
-  <style type="text/css">
-        #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: px;}
-        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
-    <form action="https://gmail.us13.list-manage.com/subscribe/post?u=dc17c244faeafae6805b365ab&amp;id=b1492a0352&amp;f_id=00ffd8e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-        <div id="mc_embed_signup_scroll"><h2>Subscribe to our Mailing List</h2>
-            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-            <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div>
-        <div id="mce-responses" class="clear foot">
-            <div class="response" id="mce-error-response" style="display: none;"></div>
-            <div class="response" id="mce-success-response" style="display: none;"></div>
-        </div>
-    <div aria-hidden="true" style="position: absolute; left: -5000px;">
-        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
-        <input type="text" name="b_dc17c244faeafae6805b365ab_b1492a0352" tabindex="-1" value="">
-    </div>
-        <div class="optionalParent">
-            <div class="clear foot">
-                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe">
-                <p style="margin: 0px auto;"><a href="http://eepurl.com/iXg-wU" title="Mailchimp - email marketing made easy and fun"><span style="display: inline-block; background-color: transparent; border-radius: 4px;"><img class="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_usimages/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"></span></a></p>
+            <div id="mailingForm">
+                <h3>Sign up for our Mailing List</h3>
+                <form action="https://api.web3forms.com/submit" method="POST">
+                    <input type="hidden" name="access_key" value="21cc2421-659f-44ea-a101-2b8f5589a8d6">
+                    <div><p>Name:</p> <input type="text" name="name" required></div>
+                    <div><p>Email:</p> <input type="email" name="email" required></div>
+                    <div><input type="checkbox" name="botcheck" class="hidden" style="display: none;"></div>
+                    <input type="hidden" name="redirect" value="https://urmomofficial.com/thank-you">
+                    <button type="submit">Submit Form</button>
+                </form>
             </div>
-        </div>
-    </div>
-</form>
-</div>
-<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script></div>
             <div class="cptou">
                 <ul>
                     <a href="/about.html#contactUs"><li>Contact</li></a>
