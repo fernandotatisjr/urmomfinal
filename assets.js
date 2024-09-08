617e42e4c9fc4c25881b2dea89515099cc95075c
fernandotatisjr <alex.omalley44@gmail.com>
2024-09-07 21:15:39 -0500

    adding scroll-up to footer

--- assets.js
+++ assets.js
@@ -1,1 +1,15 @@
 document.addEventListener("DOMContentLoaded",(function(){const e=document.querySelector(".hamburger"),t=document.querySelector(".mob-nav");e.addEventListener("click",(function(){e.classList.toggle("is-active"),t.classList.contains("is-active")?(t.classList.remove("is-active"),setTimeout((()=>{t.style.display="none"}),400)):(t.style.display="flex",setTimeout((()=>{t.classList.add("is-active")}),10)),e.classList.contains("is-active")?document.body.style.overflow="hidden":document.body.style.overflow="auto"})),window.addEventListener("resize",(()=>{window.innerWidth>950&&t.classList.contains("is-active")&&(t.classList.remove("is-active"),t.style.display="none",e.classList.remove("is-active"),document.body.style.overflow="auto")}))}));const body=document.body;let lastScroll=0;function updateElementsOnScroll(){const e=document.querySelectorAll(".scroll-element"),t=window.innerHeight,s=t/2;e.forEach((e=>{const n=e.getBoundingClientRect(),o=n.top+n.height/2;let i,l;if(o<=s)i=1,l=1;else if(n.top<t&&n.bottom>0){const e=Math.abs(s-o),n=t/2,c=Math.min(1,1-e/n);i=.8+.2*c,l=.5+.5*c}else i=.8,l=.5;e.style.transform=`scale(${i})`,e.style.opacity=l}))}window.addEventListener("scroll",(()=>{})),document.addEventListener("DOMContentLoaded",updateElementsOnScroll),document.addEventListener("scroll",updateElementsOnScroll);
+document.addEventListener('DOMContentLoaded', function () {
+const elements=document.querySelectorAll('.scroll-up');
+function checkVisibility() {
+    const scrollPosition = window.scrollY || window.pageYOffset;
+    const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    let delay = 0;
+    if(viewportWidth<=1000){delay=0;}
+    elements.forEach(element => {const elementOffset=element.getBoundingClientRect().top;if(elementOffset<=viewportHeight-delay){element.classList.add('show-up');}});
+}
+window.addEventListener('scroll', checkVisibility);
+window.addEventListener('resize', checkVisibility);
+checkVisibility();
+});
