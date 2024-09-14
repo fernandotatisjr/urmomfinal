75cc00cc3da71c2b66ef6617056d0c6cd15003a1
fernandotatisjr <alex.omalley44@gmail.com>
2024-09-14 18:59:48 -0500

    iframes with images

--- utilities/home.js
+++ utilities/home.js
@@ -1,1 +1,14 @@
+document.querySelectorAll('.video-thumbnail').forEach(thumbnail => {
+    thumbnail.addEventListener('click', function() {
+        const videoId = this.getAttribute('data-video-id');
+        const iframe = document.createElement('iframe');
+        iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1`);
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+        iframe.setAttribute('allowfullscreen', true);
+        iframe.setAttribute('loading', 'lazy');
+        this.parentNode.replaceChild(iframe, this);
+    });
+});
+
 document.querySelectorAll('.cov').forEach(cov => {cov.addEventListener('click', () => {cov.classList.toggle('show-before');});});
