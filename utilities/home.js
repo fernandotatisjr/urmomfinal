b14669f4659bee2364b6eda661d949cc9f0dd36b
fernandotatisjr <alex.omalley44@gmail.com>
2024-09-14 20:08:19 -0500

    add functionality to play button as well.

--- utilities/home.js
+++ utilities/home.js
@@ -1,15 +1,27 @@
+// Function to handle video play
+function playVideo(thumbnail) {
+    const videoId = thumbnail.getAttribute('data-video-id');
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1`);
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+    iframe.setAttribute('allowfullscreen', true);
+    iframe.setAttribute('loading', 'lazy');
+    const playButton = thumbnail.parentNode.querySelector('.play-button');
+    if (playButton) {playButton.remove();}
+    thumbnail.parentNode.replaceChild(iframe, thumbnail);
+}
 document.querySelectorAll('.video-thumbnail').forEach(thumbnail => {
     thumbnail.addEventListener('click', function() {
-        const videoId = this.getAttribute('data-video-id');
-        const iframe = document.createElement('iframe');
-        iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1`);
-        iframe.setAttribute('frameborder', '0');
-        iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
-        iframe.setAttribute('allowfullscreen', true);
-        iframe.setAttribute('loading', 'lazy');
-        const playButton = this.parentNode.querySelector('.play-button');
-        if (playButton) {playButton.remove();}
-        this.parentNode.replaceChild(iframe, this);
+        playVideo(this);
+    });
+});
+document.querySelectorAll('.play-button').forEach(button => {
+    button.addEventListener('click', function() {
+        const thumbnail = this.parentNode.querySelector('.video-thumbnail');
+        if (thumbnail) {
+            playVideo(thumbnail);
+        }
     });
 });
 
